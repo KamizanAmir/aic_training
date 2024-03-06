6e14365aa6a797c123e6eae9f9a08d9d4c641937
AICSEMICON_MY\kamizan_hidir <kamizan980505@gmail.com>
2024-03-06 15:26:40 +0800

    update fillable

--- app/Employee.php
+++ app/Employee.php
@@ -7,16 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    // protected $fillable = [
-    //     'emp_id',
-    //     'emp_name',
-    //     'department',
-    //     'labour_type',
-    //     'status',
-    //     'trainer_emp',
-    //     'training_type',
-    //     'training_hours',
-    //     'training_date',
-    //     'expired_date',
-    // ];
+    protected $fillable = [
+        'emp_id',
+        'emp_name',
+        'department',
+        'labour_type',
+        'status',
+        'trainer_emp',
+        'training_type',
+        'training_hours',
+        'training_date',
+        'expired_date',
+    ];
 }
